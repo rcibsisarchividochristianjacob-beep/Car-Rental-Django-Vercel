9f243a0a910bb7fc5d972e0443f01936de13bf29
rcibsisarchividochristianjacob-beep <rci.bsis.archividochristianjacob@gmail.com>
2026-02-02 02:56:12 +0800

    Update settings.py

--- ocrs/settings.py
+++ ocrs/settings.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = 'ocrs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join('/tmp', 'db.sqlite3'), 
     }
 }
 
