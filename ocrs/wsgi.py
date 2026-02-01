7077bb17ee605a474edbace63304e888449b628e
rcibsisarchividochristianjacob-beep <rci.bsis.archividochristianjacob@gmail.com>
2026-02-02 02:19:26 +0800

    Update wsgi.py

--- ocrs/wsgi.py
+++ ocrs/wsgi.py
@@ -1,18 +1,9 @@
-"""
-WSGI config for ocrs project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ocrs.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ocrs.settings')
 
 application = get_wsgi_application()
 
+# ITO ANG PINAKAMAHALAGANG LINYA:
 app = application
