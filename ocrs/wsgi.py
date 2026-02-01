9f77a256a09d764d33e132ec1a24ef935c263880
rcibsisarchividochristianjacob-beep <rci.bsis.archividochristianjacob@gmail.com>
2026-02-02 02:33:01 +0800

    Update wsgi.py

--- ocrs/wsgi.py
+++ ocrs/wsgi.py
@@ -5,5 +5,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ocrs.settings')
 
 application = get_wsgi_application()
 
-# ITO ANG PINAKAMAHALAGANG LINYA:
+# ITO ANG PINAKAMAHALAGANG LINYA NA NAWAWALA SA LOGS MO
 app = application
